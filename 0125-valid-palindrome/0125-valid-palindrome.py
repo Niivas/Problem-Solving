0752e563e0de9089b292fbd9371c2aceee648712
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-18 10:03:00 +0530

    Time: 63 ms (32.91%), Space: 17 MB (27.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Convert the string to lowercase
+        s = s.lower()
+
+        # Initialize an empty string to store alphanumeric characters
+        res = ""
+
+        # Iterate through each character in the string
+        for i in s:
+            # Check if the character is alphabetic or alphanumeric
+            if i.isalpha() or i.isalnum():
+                # If it is, add it to the result string
+                res += i
+        
+        # Check if the result string is equal to its reverse
+        return res == res[::-1]
