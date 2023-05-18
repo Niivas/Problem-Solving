c9e45f33102f5a54d64896940971b24e95a0a21c
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-18 15:42:54 +0530

    Time: 652 ms (94.24%), Space: 29.6 MB (12.13%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, H: List[int]) -> int:
+        # initialize the maximum area seen so far and the two pointers
+        ans, i, j = 0, 0, len(H)-1
+        
+        # iterate until the two pointers meet
+        while (i < j):
+            # compute the area between the two pointers
+            if H[i] <= H[j]:
+                res = H[i] * (j - i)
+                i += 1
+            else:
+                res = H[j] * (j - i)
+                j -= 1
+            
+            # update the maximum area seen so far
+            if res > ans:
+                ans = res
+        
+        # return the maximum area seen
+        return ans
