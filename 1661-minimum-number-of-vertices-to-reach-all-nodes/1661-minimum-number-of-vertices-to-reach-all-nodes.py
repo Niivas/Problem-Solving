ea1f461cec3dcfe8506e6ea8c2b7992ddc8843c9
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-18 10:00:38 +0530

    Time: 1188 ms (48.46%), Space: 54.2 MB (32.88%) - LeetHub

---
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        # Create a set of all vertices initially
+        vertices = set(range(n))
+
+        # Iterate through the edges
+        for i in edges:
+            # If the second vertex of an edge is in the set of vertices
+            if i[1] in vertices:
+                # Remove it from the set
+                vertices.remove(i[1])
+        
+        # Convert the set of vertices to a list and return
+        return list(vertices)
