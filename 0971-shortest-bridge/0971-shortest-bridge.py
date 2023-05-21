a8674b73dd8ab5b377471d1e2e1f044dad599050
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-21 22:14:07 +0530

    Time: 373 ms (87.42%), Space: 19 MB (45.48%) - LeetHub

---
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -0,0 +1,39 @@
+class Solution:
+        def shortestBridge(self, A: List[List[int]]) -> int:
+            bound= set()
+            dire = [(1,0),(-1,0),(0,1),(0,-1)]
+            m, n = len(A), len(A[0])
+            # get root of islanda
+            def getfirst():
+                for i in range(m):
+                    for j in range(n):
+                        if A[i][j] == 1:
+                            return i,j
+            # get boundary of islanda
+            def dfs(i,j):
+                A[i][j] = -1
+                for d in dire:
+                    x,y = i+d[0],j+d[1]
+                    if 0 <= x<m and 0 <= y < n:
+                        if A[x][y] == 0:
+                            bound.add((i,j))
+                        elif A[x][y] == 1:
+                            dfs(x,y)
+            i,j = getfirst()
+            dfs(i,j)
+            # BFS to find next island
+            step = 0
+            while bound:
+                new = []
+                for i,j in bound:
+                    for d in dire:
+                        x,y = i+d[0],j+d[1]
+                        if 0 <= x < m and 0 <= y < n:
+                            if A[x][y] == 1:
+                                return step
+                            elif A[x][y] ==0:
+                                A[x][y] = -1
+                                new.append((x,y))
+                step += 1
+                bound = new
+            
