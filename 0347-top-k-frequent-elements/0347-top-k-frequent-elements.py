0572df0b40f86b7cd5e201e2cb47cbb43361e18e
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-22 09:37:31 +0530

    Time: 105 ms (78.08%), Space: 21.2 MB (30.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,25 @@
+import heapq
+from collections import Counter
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
+        # Count the frequency of each number in the list
         counter = Counter(nums)
-        ans,maxheap = [],[]
-        for key in counter:
-            heapq.heappush(maxheap,(-counter[key],-key))
+        
+        # Initialize an empty list for the answer and a max heap
+        top_k_frequent = []
+        max_heap = []
+        
+        # Push the frequency and negated number (-frequency, -number) into the max heap
+        for num, freq in counter.items():
+            heapq.heappush(max_heap, (-freq, -num))
         
         i = 0
-
-        while i<k:
-            value,key = heapq.heappop(maxheap)
-            ans.append(-key)
-            i+=1
+        # Pop the top k elements from the max heap and append the negated number to the answer list
+        while i < k:
+            freq, num = heapq.heappop(max_heap)
+            top_k_frequent.append(-num)
+            i += 1
         
-        return ans
+        return top_k_frequent
