764afe7f6125f80457223825c9c15deab44d8b3b
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-25 17:41:35 +0530

    Time: 97 ms (41.53%), Space: 16.8 MB (9.32%) - LeetHub

---
+++ 0867-new-21-game/0867-new-21-game.py
@@ -0,0 +1,26 @@
+class Solution:
+    def new21Game(self, N: int, K: int, maxPts: int) -> float:
+        # Check if winning is guaranteed
+        if K == 0 or N >= K + maxPts:
+            return 1.0
+
+        # Initialize the probability list
+        probabilities = [1.0] + [0.0] * N
+
+        # Initialize the sum of probabilities for the last W cards
+        last_maxPts_sum = 1.0
+
+        # Calculate the probabilities for each score from 1 to N
+        for i in range(1, N + 1):
+            # Probability of achieving score i
+            probabilities[i] = last_maxPts_sum / maxPts
+
+            # Update the sum of probabilities for the last W cards
+            if i < K:
+                last_maxPts_sum += probabilities[i]
+
+            if i - maxPts >= 0:
+                last_maxPts_sum -= probabilities[i - maxPts]
+
+        # Return the sum of probabilities starting from index K
+        return sum(probabilities[K:])
