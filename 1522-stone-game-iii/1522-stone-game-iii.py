6704bcaace3f75995712f0f166b5569754e1507e
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-27 20:25:34 +0530

    Time: 2872 ms (60.54%), Space: 221.4 MB (21.97%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.py
+++ 1522-stone-game-iii/1522-stone-game-iii.py
@@ -1,24 +1,38 @@
+from typing import List
+from functools import lru_cache
+
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
         n = len(stoneValue)
 
         @lru_cache(None)
-        def game(i):
-            if i>=n:
+        def game(i: int) -> int:
+            # Base case: no more stones to pick
+            if i >= n:
                 return 0
-            
-            sum = stoneValue[i]
-            value = sum-game(i+1)
-            for j in range(i+1,min(i+3,n)):
-                sum += stoneValue[j]
-                value = max(value,sum-game(j+1))
+
+            # Calculate the sum of the current stone
+            total = stoneValue[i]
+
+            # Calculate the optimal value Alice can obtain
+            value = total - game(i + 1)
+
+            # Iterate over the different choices Alice can make
+            for j in range(i + 1, min(i + 3, n)):
+                # Update the sum with the value of the stone
+                total += stoneValue[j]
+
+                # Calculate the maximum value Alice can obtain
+                value = max(value, total - game(j + 1))
+
             return value
 
+        # Calculate the score (optimal value for Alice)
         score = game(0)
 
-        if score>0:
+        # Determine the winner based on the score
+        if score > 0:
             return "Alice"
-        elif score<0:
+        elif score < 0:
             return "Bob"
         return "Tie"
-
