a1c3d9b61627212b427b95d5b3f0cd88c1f8b371
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-05-30 21:37:56 +0530

    Time: 80 ms (35.46%), Space: 16.5 MB (34.01%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Create a counter of the characters in the ransom note
+        r_counter = Counter(ransomNote)
+        
+        # Loop through each character in the magazine
+        for i in magazine:
+            # If the character appears in the ransom note
+            if i in r_counter:
+                # Decrement the count of the character in the ransom note's counter
+                r_counter[i] -= 1
+                # If the count of the character reaches zero, remove it from the counter
+                if not r_counter[i]:
+                    del r_counter[i]
+        
+        # If all characters in the ransom note have counts of zero in the counter, return True
+        if not len(r_counter):
+            return True
+        # Otherwise, return False
+        return False
