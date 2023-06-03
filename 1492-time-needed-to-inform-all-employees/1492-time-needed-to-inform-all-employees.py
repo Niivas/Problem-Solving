9011b03449f7da14df79c7521c2b95217d9a3ad2
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-06-03 12:50:58 +0530

    Time: 1283 ms (72.98%), Space: 44.9 MB (64.05%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
@@ -2,19 +2,23 @@ class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
 
         tree = defaultdict(list)
-
-        for idx,value in enumerate(manager):
+        # Build the tree by assigning each employee to their respective manager
+        for idx, value in enumerate(manager):
             if idx != headID:
                 tree[value].append(idx)
+
         total_time = 0
+        queue = deque([(headID, informTime[headID])])
 
-        queue = deque([(headID,informTime[headID])])
-
+        # Perform breadth-first search traversal of the tree
         while queue:
-            cur_employee,cur_time = queue.popleft()
-            total_time = max(total_time,cur_time)
-            for sub_ordinate in tree[cur_employee]:
-                if tree[sub_ordinate]:
-                    queue.append((sub_ordinate,cur_time+informTime[sub_ordinate]))
-        
+            cur_employee, cur_time = queue.popleft()
+            # Update the maximum time required to inform all employees
+            total_time = max(total_time, cur_time)
+            # Visit each subordinate of the current employee
+            for subordinate in tree[cur_employee]:
+                if tree[subordinate]:
+                    # Calculate the time required to inform the subordinate and enqueue them
+                    queue.append((subordinate, cur_time + informTime[subordinate]))
+
         return total_time
