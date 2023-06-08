660fff1048441f6b66935ed88cec798b15339704
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-06-08 14:54:00 +0530

    Time: 50 ms (35.28%), Space: 16.6 MB (13.22%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+        @lru_cache(None)
+        def dfs(i):
+            if i>n:
+                return 0
+            if i==n:
+                return 1
+            if s[i]=='0':
+                return 0
+            #count=0
+            if int(s[i:i+2])<27:
+                return dfs(i+2)+dfs(i+1)
+            return dfs(i+1)
+        return dfs(0)
+
