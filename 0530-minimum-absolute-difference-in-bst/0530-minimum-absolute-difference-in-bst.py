a2944c1f2cb5d4f5b24af904c16760a28c03644a
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-06-14 12:40:28 +0530

    Time: 79 ms (15.71%), Space: 18.6 MB (42.66%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -4,32 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math
-
 class Solution:
+    prev,ans = None,math.inf
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        # Store the sorted values of the nodes
-        sorted_values = []
+        if not root:
+            return self.ans
+
+        self.getMinimumDifference(root.left)
+
+        if self.prev != None:
+            self.ans = min(self.ans,abs(root.val - self.prev))
+        self.prev = root.val
         
-        # Perform in-order traversal to populate the sorted_values list
-        def inorder(node):
-            nonlocal sorted_values
-            if not node:
-                return
-            inorder(node.left)  # Traverse left subtree
-            sorted_values.append(node.val)  # Append current node value
-            inorder(node.right)  # Traverse right subtree
-        
-        # Call inorder function to populate the sorted_values list
-        inorder(root)
-        
-        # Initialize the minimum difference to positive infinity
-        min_diff = math.inf
-        
-        # Iterate over the sorted array and calculate minimum difference
-        for i in range(1, len(sorted_values)):
-            diff = sorted_values[i] - sorted_values[i - 1]
-            min_diff = min(min_diff, diff)
-        
-        # Return the minimum difference
-        return min_diff
+        self.getMinimumDifference(root.right)
+        return self.ans
