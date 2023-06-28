bbac657acbb62abdefca109bef3164f9b6132c20
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-06-28 10:15:43 +0530

    Time: 751 ms (33.93%), Space: 29.4 MB (15.62%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,22 +1,36 @@
+from collections import defaultdict, deque
+from typing import List
+
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+    def maxProbability(self, num_nodes: int, edges: List[List[int]], success_probabilities: List[float], start_node: int, end_node: int) -> float:
+        # Create an adjacency list representation of the graph
         graph = defaultdict(list)
-        l = len(edges)
-        for i in range(l):
-            graph[edges[i][0]].append((edges[i][1],succProb[i]))
-            graph[edges[i][1]].append((edges[i][0],succProb[i]))
+        num_edges = len(edges)
+        for i in range(num_edges):
+            src, dest = edges[i]
+            prob = success_probabilities[i]
+            graph[src].append((dest, prob))
+            graph[dest].append((src, prob))
         
-        distance = [0.0]*n
-        distance[start] = 1.0
-        queue = deque([start])
+        # Initialize an array to store the maximum probabilities of reaching each node
+        max_probabilities = [0.0] * num_nodes
+        max_probabilities[start_node] = 1.0
+        
+        # Use a queue for breadth-first search
+        queue = deque([start_node])
 
+        # Perform breadth-first search to calculate maximum probabilities
         while queue:
-            cur_node = queue.popleft()
+            current_node = queue.popleft()
 
-            for neighbor,prob in graph[cur_node]:
-                new_prob =  distance[cur_node]*prob
+            # Iterate through the neighbors of the current node
+            for neighbor, edge_prob in graph[current_node]:
+                new_prob = max_probabilities[current_node] * edge_prob
 
-                if new_prob>distance[neighbor]:
-                        distance[neighbor] = new_prob
-                        queue.append(neighbor)
-        return distance[end]   
+                # If a higher probability path is found to reach the neighbor node, update the maximum probability
+                if new_prob > max_probabilities[neighbor]:
+                    max_probabilities[neighbor] = new_prob
+                    queue.append(neighbor)
+        
+        # Return the maximum probability of reaching the end node from the start node
+        return max_probabilities[end_node]
