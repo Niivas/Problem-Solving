1b2b51b73612422e74e7359f5557dd0b7f280d19
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-07-03 16:13:50 +0530

    Time: 64 ms (6.43%), Space: 16.5 MB (92.04%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        # Check if s is equal to goal and if all characters in s are unique
+        if s == goal and len(set(s)) == len(s):
+            return False
+        
+        # Check if character counts of s and goal are different
+        if Counter(s) != Counter(goal):
+            return False
+        
+        count = 0
+        i = 0
+        j = 0
+        
+        # Iterate over s and goal
+        while i < len(s) and j < len(goal):
+            if s[i] == goal[j]:
+                # Characters at current positions are equal, increment both pointers
+                i += 1
+                j += 1
+            else:
+                # Characters at current positions are different, increment count and both pointers
+                count += 1
+                i += 1
+                j += 1
+            
+            # Check if count exceeds 2, return False
+            if count > 2:
+                return False
+        
+        # Check if both pointers have reached the end of their respective strings
+        if i != len(s) or j != len(goal):
+            return False
+        
+        # Return True if all conditions are passed
+        return True
