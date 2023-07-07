120c0d4fa31b1a008ea3962950f04468024d6098
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-07-07 14:55:33 +0530

    Time: 1599 ms (5.18%), Space: 16.7 MB (85.98%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,49 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        # Initialize the search range
+        low, high = 1, len(answerKey)
+
+        def is_valid_length(length):
+            t_count = 0
+            f_count = 0
+
+            # Count occurrences of 'T' and 'F' in the first (length-1) characters
+            for i in range(length - 1):
+                if answerKey[i] == 'T':
+                    t_count += 1
+                else:
+                    f_count += 1
+
+            left, right = 0, length - 1
+            while right < len(answerKey):
+                # Update counts for the current window
+                if answerKey[right] == 'T':
+                    t_count += 1
+                else:
+                    f_count += 1
+
+                # Check if the current subsequence satisfies the condition
+                if min(t_count, f_count) <= k:
+                    return True
+                else:
+                    # Adjust counts and move the window
+                    if answerKey[left] == 'T':
+                        t_count -= 1
+                    else:
+                        f_count -= 1
+                    left += 1
+                    right += 1
+
+            return False
+
+        ans = 1
+        while low <= high:
+            mid = (low + high) >> 1  # Calculate the middle value
+
+            if is_valid_length(mid):
+                ans = mid
+                low = mid + 1  # Adjust the low pointer to search for longer subsequences
+            else:
+                high = mid - 1  # Adjust the high pointer to search for shorter subsequences
+
+        return ans
