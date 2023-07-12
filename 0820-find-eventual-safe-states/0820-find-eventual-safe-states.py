f87e8b3c814f29543ab7348f3719cca31b178e68
Nivas Reddy Nalla <89236090+Niivas@users.noreply.github.com>
2023-07-12 21:17:36 +0530

    Time: 676 ms (57.22%), Space: 23.5 MB (96.47%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isCyclic(self, adj_list, src, visited, recursion_stack):
+        visited[src] = True
+        recursion_stack[src] = True
+        for neighbor in adj_list[src]:
+            if not visited[neighbor] and self.isCyclic(adj_list, neighbor, visited, recursion_stack):
+                return True
+            elif recursion_stack[neighbor]:
+                return True
+        recursion_stack[src] = False
+        return False
+
+    def eventualSafeNodes(self, graph):
+        num_nodes = len(graph)
+        adj_list = [[] for _ in range(num_nodes)]
+
+        # Construct the adjacency list
+        for node in range(num_nodes):
+            for neighbor in graph[node]:
+                adj_list[node].append(neighbor)
+
+        visited = [False] * num_nodes
+        recursion_stack = [False] * num_nodes
+
+        # Perform DFS on all nodes to detect cycles
+        for node in range(num_nodes):
+            if not visited[node]:
+                self.isCyclic(adj_list, node, visited, recursion_stack)
+
+        eventual_safe_nodes = []
+        for node in range(len(recursion_stack)):
+            if not recursion_stack[node]:
+                eventual_safe_nodes.append(node)
+        return eventual_safe_nodes
