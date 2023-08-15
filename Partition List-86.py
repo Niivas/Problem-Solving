ccfb1bce71d3d2fd84f86d9761210a0e94534e37
Nivas Reddy Nalla <nivasreddy@logistiq.io>
2023-08-15 08:33:56 +0530

    Partition List Problem-86 Leetcode

---
+++ Partition List-86.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def partition(self,head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        valuesGreaterThanX = []
+        valuesLesserThanX  = []
+
+        start = head
+        while start:
+            if start.val >= x:
+                valuesGreaterThanX.append(start.val)
+            else:
+                valuesLesserThanX.append(start.val)
+            start = start.next
+
+        ans = head
+
+        for i in valuesLesserThanX:
+            head.val = i
+            head = head.next
+        for i in valuesGreaterThanX:
+            head.val = i
+            head = head.next
+        return  ans
