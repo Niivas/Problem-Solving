230fe215a7d980e7e53db512e24a46d20003ebb8
Nivas Reddy Nalla <nivasreddy@logistiq.io>
2023-08-15 09:06:21 +0530

    Updated problem - 643 with comments

--- Maximum Average Subarray I - 643.py
+++ Maximum Average Subarray I - 643.py
@@ -2,14 +2,30 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # Initialize the current sum as the sum of the first (k-1) elements
         curSum = sum(nums[:k-1])
-        left,right = 0,k-1
+
+        # Initialize pointers for the left and right boundaries of the subarray
+        left, right = 0, k - 1
+
+        # Initialize the maximum sum with negative infinity
         maxSum = -float('inf')
-        while right<len(nums):
+
+        # Iterate through the array while the right boundary is within bounds
+        while right < len(nums):
+            # Add the current element to the current sum
             curSum += nums[right]
-            if curSum>maxSum:
+
+            # Update the maximum sum if the current sum is greater
+            if curSum > maxSum:
                 maxSum = curSum
-            curSum-= nums[left]
-            left+=1
-            right +=1
-        return maxSum/k
+
+            # Subtract the leftmost element from the current sum
+            curSum -= nums[left]
+
+            # Move the left and right boundaries one step to the right
+            left += 1
+            right += 1
+
+        # Return the maximum average by dividing the maximum sum by k
+        return maxSum / k
