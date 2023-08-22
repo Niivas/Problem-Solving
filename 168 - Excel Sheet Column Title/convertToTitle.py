d9ce0d4f408bdab28b9af28cdff7bccf26f4a8bc
Niivas <nivas6900@gmail.com>
2023-08-22 07:49:33 +0530

    Leetcode problem - 168

---
+++ 168 - Excel Sheet Column Title/convertToTitle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = ""  # Initialize an empty string to store the resulting column title
+
+        while columnNumber:
+            columnNumber -= 1  # Decrement columnNumber by 1 for 1-based indexing
+
+            # Calculate the character corresponding to the current column number
+            # Modulo operation gives position in the alphabet, adding 65 converts to ASCII
+            current_char = chr((columnNumber % 26) + 65)
+
+            ans += current_char  # Append the calculated character to the ans string
+            columnNumber //= 26  # Integer divide columnNumber by 26 to move left
+
+        return ans[::-1]  # Reverse the ans string before returning the final result
