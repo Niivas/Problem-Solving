ee3e7974118527df466bfe6d7ac674940ecc91d3
Niivas <nivas6900@gmail.com>
2023-09-03 11:56:47 +0530

    Leetcode problem - 62

---
+++ 62 - Unique Paths/uniquePaths.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # create a 2D grid of size m x n initialized with zeros
+        dp = [[0]*n for _ in range(m)]
+
+        # iterate over each cell in the grid
+        for i in range(m):
+            for j in range(n):
+                # if we are in the first row or the first column, there is only one way to reach this cell
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    # otherwise, the number of unique paths that lead to this cell is the sum of the number
+                    # of unique paths that lead to the cell above it and the cell to the left of it
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        # return the number of unique paths that lead to the bottom-right corner of the grid
+        return dp[m-1][n-1]
