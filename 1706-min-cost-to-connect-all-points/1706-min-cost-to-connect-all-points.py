b339dc908a758c1522bf61380fa14c46adeaa0de
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-09-15 21:55:58 +0530

    Time: 1356 ms (72%), Space: 112.1 MB (56.81%) - LeetHub

---
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -0,0 +1,20 @@
+class Solution:
+        def minCostConnectPoints(self, p: List[List[int]]) -> int:
+        
+            def manhattan(x, y):
+                return abs(x[0]-y[0]) + abs(x[1]-y[1])
+            
+            ans, n = 0, len(p)
+            seen = set()
+            vertices = [(0, (0, 0))]
+            
+            while len(seen) < n:
+                # print(vertices, seen)
+                w, (u, v) = heapq.heappop(vertices)            
+                if v in seen: continue
+                ans += w
+                seen.add(v)
+                for j in range(n):
+                    if j not in seen and j!=v:
+                        heapq.heappush(vertices, (manhattan(p[j], p[v]), (v, j)))
+            return ans
