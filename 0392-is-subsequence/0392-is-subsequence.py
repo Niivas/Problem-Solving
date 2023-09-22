09fdc4415ad748b7733552f7830071610c7aa51b
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-09-22 08:14:02 +0530

    Time: 38 ms (70.22%), Space: 16.3 MB (84.5%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,19 +1,14 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        lenS = len(s)  # Length of string s
-        lenT = len(t)  # Length of string t
-        
-        if lenS > lenT:
-            return False  # If s is longer than t, it cannot be a subsequence
-        
-        p1, p2 = 0, 0  # Pointers for string s and t
-        
-        while p1 < lenS and p2 < lenT:
-            if s[p1] == t[p2]:  # If characters match, move pointer p1
-                p1 += 1
-            p2 += 1  # Move pointer p2 regardless
-            
+        lenS = len(s)
+        lenT = len(t)
+        if lenS>lenT:
+            return False
+        p1,p2 = 0,0
+        while(p1<lenS and p2<lenT):
+            if s[p1] == t[p2]:
+                p1 +=1
+            p2 +=1
         if p1 == lenS:
-            return True  # If p1 reached the end of s, s is a subsequence of t
-        
-        return False  # Otherwise, s is not a subsequence of t
+            return True
+        return False
