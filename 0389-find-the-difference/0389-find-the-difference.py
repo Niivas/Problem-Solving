bead14284dc145f9a728970e49f61ac2a3653ebd
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-09-25 19:57:02 +0530

    Time: 38 ms (78.35%), Space: 16.1 MB (99.59%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        c1 = Counter(s)
+        c2 = Counter(t)
+
+        for i in t:
+            if i not in c1:
+                return i
+            if c2[i]>c1[i]:
+                return i
