04a7c723a07146cf86ff1d204b2e8bc8937b9c5e
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-04 08:45:33 +0530

    Time: 203 ms (72.42%), Space: 20 MB (58.72%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,42 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 10**4
+        self.table = [0]*self.size
+
+    def put(self, key: int, value: int) -> None:
+        idx = key%(self.size)
+        if self.table[idx] == 0:
+            self.table[idx] = [[key,value]]
+        else:
+            keyFound = False
+            for i in range(len(self.table[idx])):
+                if self.table[idx][i][0] == key:
+                    self.table[idx][i][1] = value
+                    keyFound = True
+            if not keyFound:
+                self.table[idx].append([key,value])
+
+    def get(self, key: int) -> int:
+        idx = key%self.size
+        if self.table[idx] == 0:
+            return -1
+        for pair in self.table[idx]:
+            if pair[0] == key:
+                return pair[1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        idx = key%self.size
+        if self.table[idx] != 0:
+            for pair in self.table[idx]:
+                if pair[0] == key:
+                    self.table[idx].remove(pair)
+                    break
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
