c233d5a6c21065a83324de7967aca1a1a8d5a9c9
Niivas <nivas6900@gmail.com>
2023-10-05 13:06:48 +0530

    Modified getMinMachines code

--- machines required/getMinMachines.py
+++ machines required/getMinMachines.py
@@ -1,24 +1,21 @@
-def getMinMachines():
-    pass
+def getMinMachines(start, end):
+    intervals = list(zip(start, end))
+    map_ = {}  # 1:9, 2:14, 3:7
+
+    for pair in intervals:
+        s, e = pair[0], pair[1]
+        found = False
+        for m in map_:
+            if map_[m] < s:
+                map_[m] = e
+                found = True
+                break
+        if not found:
+            map_[len(map_) + 1] = e
+    return len(map_)
 
 
-n = 5
-start = [1, 8, 3, 9, 6]
-end = [7, 9, 6, 14, 7]
+intervalStart = [1, 3, 0, 5, 8, 5]
+intervalEnd = [2, 4, 6, 7, 9, 9]
 
-intervals = [(1, 7), (3, 6), (6, 7), (8, 9), (9, 14)]
-
-map_ = {}  # 1:9, 2:14, 3:7
-
-for pair in intervals:
-    s, e = pair[0], pair[1]
-    found = False
-    for m in map_:
-        if map_[m] < s:
-            map_[m] = e
-            found = True
-            break
-    if not found:
-        map_[len(map_) + 1] = e
-
-print(len(map_))
+print(getMinMachines(intervalStart, intervalEnd))
