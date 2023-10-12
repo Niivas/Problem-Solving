a54a3ba3e82b0221c49cfc3123e9e89939326231
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-12 07:20:14 +0530

    Time: 37 ms (70.08%), Space: 17.1 MB (90.39%) - LeetHub

---
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.py
@@ -0,0 +1,40 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+        def findInMountainArray(self, target, A):
+            n = A.length()
+            # find index of peak
+            l, r = 0, n - 1
+            while l < r:
+                m = (l + r) // 2
+                if A.get(m) < A.get(m + 1):
+                    l = peak = m + 1
+                else:
+                    r = m
+            # find target in the left of peak
+            l, r = 0, peak
+            while l <= r:
+                m = (l + r) // 2
+                if A.get(m) < target:
+                    l = m + 1
+                elif A.get(m) > target:
+                    r = m - 1
+                else:
+                    return m
+            # find target in the right of peak
+            l, r = peak, n - 1
+            while l <= r:
+                m = (l + r) // 2
+                if A.get(m) > target:
+                    l = m + 1
+                elif A.get(m) < target:
+                    r = m - 1
+                else:
+                    return m
+            return -1
