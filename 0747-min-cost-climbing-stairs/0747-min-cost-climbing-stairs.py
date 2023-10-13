fec9dbd48ca51506de970622cd2768e05ddd516c
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-13 09:06:01 +0530

    Time: 46 ms (99.48%), Space: 16.5 MB (52.54%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Initialize variables to represent the cost of reaching the first and second stairs.
+        a, b = cost[0], cost[1]
+        # Get the total number of stairs.
+        n = len(cost)
+        # Iterate through all stairs, starting from the third stair.
+        for i in range(2, n):
+            # Update the cost of reaching the current stair.
+            # The cost of reaching the current stair is equal to the cost of the current stair
+            # plus the minimum of the cost of reaching the previous stair and the cost of reaching the stair before the previous stair.
+            a, b = b, cost[i] + min(a, b)
+        # The minimum cost of reaching the top of the stairs is the minimum of the cost of reaching the last two stairs.
+        return min(a, b)
