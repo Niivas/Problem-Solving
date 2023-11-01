75d20790c148c01e52468d23e2da6ce72f81fee9
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-11-01 07:49:54 -0400

    Time: 62 ms (26%), Space: 20.7 MB (36.54%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        # 1. Initialize a dictionary to store the frequency of each node
+        freq = {}
+
+        # 2. Define a helper function to traverse the tree in-order
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            freq[node.val] = freq.get(node.val, 0) + 1
+            inorder(node.right)
+        
+        # 3. Traverse the tree in-order
+        inorder(root)
+        
+        # 4. Find the mode(s)
+        max_freq = max(freq.values())
+        return [k for k, v in freq.items() if v == max_freq]
