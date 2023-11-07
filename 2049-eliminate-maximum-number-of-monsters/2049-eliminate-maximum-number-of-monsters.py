ee835d6f0b09016b887cf2e0fa734d74c9e1e3b1
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-11-07 11:15:50 -0500

    Time: 725 ms (28.68%), Space: 32.1 MB (62.27%) - LeetHub

---
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,12 @@
+from heapq import heappush, heappop
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        minHeap = []
+        for i in range(len(dist)):
+            heappush(minHeap, dist[i] / speed[i])
+        count = 0
+        while minHeap:
+            if count >= heappop(minHeap):
+                return count
+            count += 1
+        return count
